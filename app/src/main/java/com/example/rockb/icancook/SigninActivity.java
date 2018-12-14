ecc7fc2168c9e85fe7bab99a8cf435d533cb5327
Shane Harris <shaneharris@csus.edu>
2018-12-14 10:29:33 -0800

    Three buttons linked to compiler

--- app/src/main/java/com/example/rockb/icancook/SigninActivity.java
+++ app/src/main/java/com/example/rockb/icancook/SigninActivity.java
@@ -2,12 +2,18 @@ package com.example.rockb.icancook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SigninActivity extends AppCompatActivity {
-
+    Button googleButton, facebookButton, createButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        googleButton = findViewById(R.id.googleButton);
+        facebookButton = findViewById(R.id.facebookButton);
+        createButton = findViewById(R.id.createButton);
+
     }
 }
